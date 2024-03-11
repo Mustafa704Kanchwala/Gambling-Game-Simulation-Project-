05776f6edc7fcc3a3d50e6eecb973ba1943d736a
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-03-11 16:01:43 +0530

    [MODIFIED]:MUSTAFA - UC-1-Gambling Stack and Bet Limit

---
+++ src/com/mustafa/GamblingSim/package-info.java
@@ -0,0 +1,11 @@
+package com.mustafa.GamblingSim;
+
+import java.util.*;
+class GamblingGame{
+	public static void main() {
+		final int DAILY_STACK = 100;
+		final int BET_PER_GAME = 1;
+		
+		
+	}
+}
