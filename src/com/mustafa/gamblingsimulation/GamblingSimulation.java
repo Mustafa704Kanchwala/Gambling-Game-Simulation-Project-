7fae64cd27e8b36d0d331c6c7a2c1188b8e9b00f
Mustafa704Kanchwala <gitsquad704@gmail.com>
2024-03-13 17:20:40 +0530

    [MODIFIED]:MUSTAFA - UC-3-Refactored Code and Setting the Playing limt for Gambler

--- src/com/mustafa/gamblingsimulation/GamblingSimulation.java
+++ src/com/mustafa/gamblingsimulation/GamblingSimulation.java
@@ -15,25 +15,31 @@ public class GamblingSimulation {
 		System.out.println("Bet per Game: " + BET_PER_GAME);
 	}
 	public static void placeBet() {
-		Random random = new Random();
-		bet = random.nextInt(9)%2;
-		
-		if(bet == WIN) {		
-			System.out.println("BET SUCCESS!!");
-			win_stake = win_stake + 1;
+
+		while(true) {
+			if(win_stake == DAILY_STACK / 2  || loose_stake == DAILY_STACK / 2) {
+				System.out.println("GAMBLER RESINGED......");
+				break;
+			}
+			else {
+				gameStatus();
+				Random random = new Random();
+				bet = random.nextInt(9)%2;
+				if(bet == WIN) {		
+					System.out.println("BET SUCCESS!!");
+					win_stake = win_stake + 1;
+				}
+				else {
+					System.out.println("BET FAILED!!");
+					loose_stake++;
+				}
+				System.out.println("Winning Stake ="+win_stake);
+				System.out.println("Loosing Stake ="+loose_stake);
+				System.out.println("--------------------");	
+			}
 		}
-		else {
-			System.out.println("BET FAILED!!");
-			loose_stake++;
-		}
-		System.out.println("Winning Stake ="+win_stake);
-		System.out.println("Loosing Stake ="+loose_stake);
-		System.out.println("--------------------");	
 	}
 	public static void main(String args[]) {
-		gameStatus();
-		
 		placeBet();
-		
 	}
 }
